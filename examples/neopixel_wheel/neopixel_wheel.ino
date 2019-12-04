1c9c273584d0abf38e29222854b7a3a9069e1ddd
Mauro Alfieri <mauroalfieri@MacBook-Pro-di-Mauro-3.local>
2019-12-04 11:09:00 +0100

    First push

---
+++ examples/neopixel_wheel/neopixel_wheel.ino
@@ -0,0 +1,69 @@
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+#define encA 3
+#define encB 4
+#define swtc 1
+#define PIN  2
+#define NUMPIXELS      24
+
+volatile byte currentValue = 0;
+volatile int  lastEncoded  = 0;
+volatile byte mybrightness = 64;
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+    pinMode(encA, INPUT);
+    pinMode(encB, INPUT);
+    pinMode(swtc, INPUT);
+    digitalWrite(encA, HIGH);
+    digitalWrite(encB, HIGH);
+   
+    GIMSK = 0b00100000;       // Enable pin change interrupts
+    PCMSK = 0b00011010;       // Enable pin change interrupt for PB3 and PB4 and PB1
+    sei();                    // Turn on interrupts
+
+    pixels.setBrightness(mybrightness);
+    pixels.begin();
+}
+ 
+void loop() {
+    byte numLed = (currentValue*NUMPIXELS/64);
+    byte loop_n = (numLed/NUMPIXELS);
+
+    for(int i=0;i<numLed;i++) { pixels.setPixelColor(i, Wheel(((loop_n * 256 / NUMPIXELS) + currentValue) & 255)); }
+    for(int i=numLed;i<NUMPIXELS;i++) { pixels.setPixelColor(i, 0,0,0); }
+    pixels.show(); // This sends the updated pixel color to the hardware.
+
+    delay(20);
+}
+
+
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return pixels.Color(255 - WheelPos * 3, 0, WheelPos * 3,0);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return pixels.Color(0, WheelPos * 3, 255 - WheelPos * 3,0);
+  }
+  WheelPos -= 170;
+  return pixels.Color(WheelPos * 3, 255 - WheelPos * 3, 0,0);
+}
+
+ISR(PCINT0_vect) {
+  byte MSB = digitalRead(encA); //MSB = most significant bit
+  byte LSB = digitalRead(encB); //LSB = least significant bit
+  
+  int encoded = (MSB << 1) |LSB; //converting the 2 pin value to single number
+  int sum  = (lastEncoded << 2) | encoded; //adding it to the previous encoded value
+ 
+  if(sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011)  currentValue++;
+  if(sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000)  currentValue--;
+  
+  lastEncoded = encoded; //store this value for next time
+}
